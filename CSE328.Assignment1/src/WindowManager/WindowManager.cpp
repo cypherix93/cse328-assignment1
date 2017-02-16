85da309e545274f63321b1512ffc9b7e63e04b3f
cypherix93 <bikram.ghosh93@gmail.com>
2017-02-15 21:56:18 -0500

    Added window manager functions to set up a stress free environment.

--- CSE328.Assignment1/src/WindowManager/WindowManager.cpp
+++ CSE328.Assignment1/src/WindowManager/WindowManager.cpp
@@ -21,7 +21,7 @@ void WindowManager::Init(int argc, char** argv) const
 void WindowManager::OpenWindow() const
 {
     glutCreateWindow("Rotating 3D Cuboid");
-    
+
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING); //Enable lighting
@@ -29,4 +29,61 @@ void WindowManager::OpenWindow() const
     glEnable(GL_LIGHT1); //Enable light #1
     glEnable(GL_NORMALIZE); //Automatically normalize normals
                             //glShadeModel(GL_SMOOTH); //Enable smooth shading
+}
+
+void WindowManager::Start(int fps)
+{
+    _FPS = fps;
+
+    // Setup Draw Handler
+    if (_DrawHandler != nullptr)
+    {
+        std::bind(glutDisplayFunc, _DrawHandler);
+    }
+
+    // Setup Event Handlers
+    if (_ResizeHandler != nullptr)
+    {
+        std::bind(glutReshapeFunc, _ResizeHandler);
+    }
+    if (_KeypressHandler != nullptr)
+    {
+        std::bind(glutKeyboardFunc, _KeypressHandler);
+    }
+
+    // Setup Update Handler
+    if (_UpdateHandler != nullptr)
+    {
+        _UpdateHandler(0);
+    }
+
+    glutMainLoop();
+}
+
+void WindowManager::OnUpdate(void(*callback)(int))
+{
+    auto delay = 1000 / _FPS;
+
+    _UpdateHandler = [callback, delay](int value)
+    {
+        callback(value);
+
+        glutPostRedisplay();
+        std::bind(glutTimerFunc, delay, _UpdateHandler, 0);
+    };
+}
+
+void WindowManager::OnDraw(void(*callback)())
+{
+    _DrawHandler = callback;
+}
+
+void WindowManager::OnResize(void(*callback)(int, int))
+{
+    _ResizeHandler = callback;
+}
+
+void WindowManager::OnKeypress(void(*callback)(unsigned char key, int x, int y))
+{
+    _KeypressHandler = callback;
 }
--- CSE328.Assignment1/src/WindowManager/WindowManager.h
+++ CSE328.Assignment1/src/WindowManager/WindowManager.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <GL/glut.h>
+#include <functional>
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
+
+#define FPS 60
 
 class WindowManager
 {
@@ -10,7 +13,24 @@ class WindowManager
     WindowManager();
     virtual ~WindowManager();
 
+    // Lifecycle functions
     void Init(int argc, char** argv) const;
     void OpenWindow() const;
+    void Start(int fps = FPS);
+
+    // Event Handler Register functions
+    void OnUpdate(void(*callback)(int));
+    void OnDraw(void(*callback)(void));
+    void OnResize(void(*callback)(int, int));
+    void OnKeypress(void(*callback)(unsigned char, int, int));
+
+
+    private:
+    int _FPS = FPS;
+
+    std::function<void(int)> _UpdateHandler = nullptr;
+    std::function<void(void)> _DrawHandler = nullptr;
+    std::function<void(int, int)> _ResizeHandler = nullptr;
+    std::function<void(unsigned char, int, int)> _KeypressHandler = nullptr;
 };
 
