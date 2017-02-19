9f1da643244c89fea3dc81061c6e4cddc2835918
cypherix93 <bikram.ghosh93@gmail.com>
2017-02-19 10:46:53 -0500

    Event handlers doesnt make snese.

--- CSE328.Assignment1/src/Application/Application.h
+++ CSE328.Assignment1/src/Application/Application.h
@@ -1,13 +1,18 @@
 #pragma once
 #include <iostream>
+#include <list>
 #include "../WindowManager/WindowManager.h"
 #include <SDL.h>
 #include <GL/freeglut.h>
+#include "../Drawing/Pixel/Pixel.h"
 
 using namespace std;
+using namespace Drawing;
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
+
+static list<Pixel> pixelsToDraw;
 
 class Application
 {
@@ -22,3 +27,9 @@ class Application
     private:
     WindowManager* _WindowManager;
 };
+
+void InitHandler();
+void DrawHandler();
+void UpdateHandler();
+void ResizeHandler(SDL_WindowEvent);
+void MouseButtonHandler(SDL_MouseButtonEvent);
